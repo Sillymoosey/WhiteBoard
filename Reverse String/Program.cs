9b7cdad353e5d2b8e2fbece03fdf8bc2c199bc27
Jack Santry <Joseph.santry@outlook.com>
2020-07-14 11:57:37 -0700

    Added methods for compare, and reverse

---
+++ Reverse String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Reverse_String
+{
+    class Program
+    {
+		// input string cat
+		// return string tac
+		public static string mirror(string normal)
+		{
+			string inputNormal = "";
+			for (int i = normal.Length; i > 0;)
+			{
+				inputNormal += normal[--i];
+			}
+			return inputNormal;
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("input your string.");
+			string input = Console.ReadLine();
+			Console.WriteLine(mirror(input));
+		}
+	}
+}
