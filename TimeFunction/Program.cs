6326cfd28e2926c7aa017033e5dc0109f3209835
Jack Santry <Joseph.santry@outlook.com>
2020-08-24 07:43:42 -0700

    Add a basic web app for converting miles and kilometers/ needs style sheet upgrade.

--- TimeFunction/Program.cs
+++ TimeFunction/Program.cs
@@ -9,9 +9,21 @@ namespace TimeFunction
             int totalMinutes = DateTime.Compare(Time1, Time2);
             return totalMinutes;
         }
+        public static int Julian(DateTime Pax)
+        {
+            int day = Pax.Day;
+            
+            //int jDate = Convert.ToInt32(julianDate);
+            //int day = jDate % 1000;
+            //int year = (jDate - day) / 1000;
+            //var date1 = new DateTime(year, 1, 1);
+            //var result = date1.AddDays(day - 1);
+            return day;
+        }
         static void Main(string[] args)
         {
-        
+            DateTime Time2 = new DateTime(2020, 2, 2);
+            Console.WriteLine(Julian(Time2));
         }
     }
 }
